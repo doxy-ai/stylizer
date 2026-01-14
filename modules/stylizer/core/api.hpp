7852fccd3ca2bb06a105c11af5f90961d25c2d1d
Doxy <doxy@doxy.fyi>
2026-01-13 16:08:04 -0800

    [Input] Initial Input System
    System to support creating reactive mappings to key codes.
    
    Add a few simple combinators on top of the reactive keys.
    
    Also implements a basic time management system in core.

--- modules/stylizer/core/api.hpp
+++ modules/stylizer/core/api.hpp
@@ -32,4 +32,39 @@ namespace stylizer {
 		}
 	};	
 
+	struct time {
+		float total = 0, delta = 0, smoothed_delta = 0;
+		size_t frame = 0;
+
+		template<typename Tprecision = std::micro, typename Tclock = std::chrono::system_clock>
+		inline time& compute_delta() {
+			static auto last_time = Tclock::now();
+			auto now = Tclock::now();
+			auto dt_precise = std::chrono::duration_cast<std::chrono::duration<int64_t, Tprecision>>(now - last_time).count();
+			delta = stdmath::stl::float64_t(dt_precise * Tprecision::num) / Tprecision::den;
+			last_time = now;
+			return *this;
+		}
+
+		inline time& increment_frame() {
+			++frame;
+			return *this;
+		}
+
+		template<typename Tprecision = std::micro, typename Tclock = std::chrono::system_clock>
+		inline time& compute_delta_and_frame() {
+			compute_delta<Tprecision, Tclock>();
+			increment_frame();
+			return *this;
+		}
+
+		template<typename Tprecision = std::micro, typename Tclock = std::chrono::system_clock>
+		inline time& update(float alpha = .9) {
+			compute_delta_and_frame<Tprecision, Tclock>();
+			total += delta;
+			smoothed_delta = alpha * smoothed_delta + (1 - alpha) * delta;
+			return *this;
+		}
+	};
+
 }
